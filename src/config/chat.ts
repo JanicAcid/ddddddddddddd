54914884ec2fe0da6c0ecd07db64c9a2852f3a53
Z User <z@container>
2026-04-05 20:53:54 +0000

    feat: add Telegram chat widget

---
+++ src/config/chat.ts
@@ -0,0 +1,4 @@
+export const TELEGRAM_BOT_TOKEN = 'REDACTED'
+// Will be set after operator writes /start to the bot
+export const OPERATOR_CHAT_ID = ''
+export const BOT_USERNAME = '@spbmarkirovka_bot'
